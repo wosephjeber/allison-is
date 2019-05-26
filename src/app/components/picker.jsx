bf3e9a851238c7d30b968cfa977c1d2b79c2d6e8
Joe Weber <wosephjeber@gmail.com>
2019-05-25 22:49:30 -0500

    Animate width during picker transition

--- src/app/components/picker.jsx
+++ src/app/components/picker.jsx
@@ -16,7 +16,22 @@ class Picker extends Component {
 
     this.state = {
       editing: false,
+      width: undefined,
       selectedIndex: 0
+    }
+  }
+
+  componentDidUpdate(_prevProps, prevState) {
+    if (!prevState.editing && this.state.editing) {
+      const { width } = this.innerElem.getBoundingClientRect()
+
+      this.setState({ width })
+    }
+
+    if (prevState.editing && !this.state.editing) {
+      const { width } = this.attributeElem.getBoundingClientRect()
+
+      this.setState({ width })
     }
   }
 
@@ -29,7 +44,9 @@ class Picker extends Component {
   }
 
   handleClickEdit = () => {
-    this.setState({ editing: true })
+    const { width } = this.attributeElem.getBoundingClientRect()
+
+    this.setState({ editing: true, width })
   }
 
   handleIncrement = () => {
@@ -41,19 +58,28 @@ class Picker extends Component {
   } 
 
   render() {
-    const { editing, selectedIndex } = this.state
+    const { editing, selectedIndex, width } = this.state
 
     if (!editing) return (
-      <button class="btn--reset" onClick={this.handleClickEdit}>
-        {ATTRIBUTES[selectedIndex]}
-      </button>
+      <div key="picker" class="slot-picker" style={{ width: `${width}px` }}>
+        <div class="slot-picker--input-outline" />
+        <button
+          class="btn--reset slot-picker--attribute"
+          ref={ref => {
+            if (ref) this.attributeElem = ref
+          }}
+          onClick={this.handleClickEdit}
+        >
+          {ATTRIBUTES[selectedIndex]}
+        </button>
+      </div>
     )
 
     const totalHeight = ATTRIBUTES.length * ITEM_HEIGHT;
     const itemOffset = ((totalHeight - (selectedIndex * ITEM_HEIGHT * 2)) / 2) - (ITEM_HEIGHT / 2)
 
     return (
-      <div class="slot-picker">
+      <div key="picker" class="slot-picker" style={{ width: `${width}px` }}>
         <div class="slot-picker--input-outline" />
         <button
           class="slot-picker--btn"
@@ -77,10 +103,14 @@ class Picker extends Component {
         >
           <nav
             class="slot-picker--inner"
+            ref={ref => {
+              if (ref) this.innerElem = ref
+            }}
             style={{ top: `${itemOffset}em` }}
           >
             {ATTRIBUTES.map((attribute, index) => (
               <a
+                class="slot-picker--attribute"
                 key={attribute}
                 onClick={() => {
                   this.handleClickAttribute(index)
