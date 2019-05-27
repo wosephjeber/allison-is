5ec1600a2a5d95b5a5e90be608117f7869b4480f
Joe Weber <wosephjeber@gmail.com>
2019-05-26 23:27:52 -0500

    Prep for production builds

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,42 @@
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
-module.exports = {
-  entry: {
-    'app': './src/app/index.js'
-  },
-  output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    compress: true,
-    port: 9000,
-    historyApiFallback: true
-  },
-  resolve: {
-    extensions: ['.js', '.jsx', '.scss']
-  },
-  module: {
-		rules: [
-			{
-				test: /\.jsx?$/,
-				exclude: /node_modules/,
-				use: 'babel-loader'
-      },
-      {
-        test: /\.scss$/,
-        exclude: /node_modules/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader']
-      }
+module.exports = function(env = {}, _argv) {
+  return {
+    entry: {
+      'app': './src/app/index.js'
+    },
+    output: {
+      filename: '[name]-[contentHash].js',
+      path: path.resolve(__dirname, 'dist')
+    },
+    devServer: {
+      contentBase: path.join(__dirname, 'dist'),
+      compress: true,
+      port: 9000,
+      historyApiFallback: true
+    },
+    resolve: {
+      extensions: ['.js', '.jsx', '.scss']
+    },
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          use: 'babel-loader'
+        },
+        {
+          test: /\.scss$/,
+          exclude: /node_modules/,
+          use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader']
+        }
+      ]
+    },
+    plugins: [
+      new MiniCssExtractPlugin({ filename: '[name]-[contentHash].css' }),
+      new HtmlWebpackPlugin({ template: './src/index.html' })
     ]
-  },
-  plugins: [
-    new MiniCssExtractPlugin({ filename: '[name].css' })
-  ]
+  };
 };
